0e052979e62097121ed3ab7e5c37069ce4b79c52
EDDIB Adil Jad Allah <jad.eddib@gmail.com>
2018-01-16 21:42:58 +0000

    get repos request

---
+++ app/src/main/java/com/jadeddib/trendingrepos/network/GetReposRequest.java
@@ -0,0 +1,22 @@
+package com.jadeddib.trendingrepos.network;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+/**
+ * Created by jad on 16/01/2018.
+ */
+
+public class GetReposRequest extends StringRequest {
+
+    public static final String GET_REPOS_REQUEST_URL = "https://api.github" +
+            ".com/search/repositories?q=created:>";
+
+    public GetReposRequest(String startDate, Response.Listener<String> listener,
+                           Response.ErrorListener errorListener) {
+
+        super(Method.GET, GET_REPOS_REQUEST_URL + startDate + "&sort=stars&order=desc", listener,
+                errorListener);
+    }
+
+}
